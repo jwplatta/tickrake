25e44bdc93202f48f197ac683790d6d9228e0ceb
Joseph Platta <jwplatta@gmail.com>
2026-09-04 12:44:00 -0500

    refactor: simplify DB.connection to a true singleton

--- lib/tickrake/db/connection.rb
+++ lib/tickrake/db/connection.rb
@@ -2,17 +2,14 @@
 
 module Tickrake
   module DB
-    @connections = {}
-    @mutex = Mutex.new
-
     class << self
       def connection(path)
-        @mutex.synchronize do
-          @connections[path] ||= SQLite3::Database.new(path).tap do |d|
-            d.results_as_hash = true
-            d.busy_timeout(5_000)
-            d.execute("PRAGMA journal_mode = WAL")
-          end
+        return @connection if defined?(@connection)
+
+        @connection = SQLite3::Database.new(path).tap do |d|
+          d.results_as_hash = true
+          d.busy_timeout(5_000)
+          d.execute("PRAGMA journal_mode = WAL")
         end
       end
     end
